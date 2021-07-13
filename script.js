6643e89617e70b0697536059152445f1b44a0e0a
Lana <lanaoglesby@gmail.com>
2021-07-13 10:44:56 -0400

    -

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-var userGrid = [8][8];
-var computerGrid = [8][8];
+var userGrid = [];
+var computerGrid = [];
 
 // Grid creations
 for(var i = 0; i < 8; i++)
 {
+    var userTempArray = []
+    var computerTempArray = []
     for(var j = 0; j < 8; j++)
     {
     //making a new div for the player grid
         var d = document.createElement("div");
         d.classList.add('grid');
-        userGrid[i][j] = d;
+        userTempArray.push(d);
     //making a new div for the computer grid    
         d = document.createElement("div");
         d.classList.add('grid');
-        computerGrid[i][j]
+        computerTempArray.push(d);
     }
+    userGrid.push(userTempArray);
+    computerGrid.push()
 }
 
 //computer randomly places ships
