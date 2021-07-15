fa0fef74f094a19f8e9e09ba2bbf29822f3f9074
Lana <lanaoglesby@gmail.com>
2021-07-15 11:58:16 -0400

    -

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-//Clicking & Selecting
+
+for(let row of rowElements) 
+{
+    let rowArray = [];
+    board.push(rowArray);
+    let squareHolder = row.getElementsByClassName("col");
+    for(let square of squareHolder) {
+        square.addEventListener("click", chosen, false);
+        var boatSegment = {cell: col, isOccupied: false, firedUpon: false}
+        rowArray.push(boat);
+        col.boat = boat;
+    }
+}
     
-//Buttons at the bottom
+////////////////////Clicking & Selecting//////////////////////////////////////////////////////////////////////////////
+function chosen()
+{
+
+}
+//////////////////////////Buttons at the bottom///////////////////////////////////////////////////////////////////////////
 
 //Randomizing for computer
+var carrier = {name: "carrier", length: 5};
+var battleship = {name: "Battleship", length: 4};
+var cruiser = {name: "Cruiser", length: 3};
+var submarine = {name: "Submarine", length: 3};
+var destroyer = {name: "Destroyer", length: 2};
 
-//Computer randomly guess*
+var allShips = [carrier, battleship, cruiser, submarine, destroyer]; 
+while(allShips.length > 0)
+{
+
+}
+
+
+//////////////////////////////////Computer randomly guess*/////////////////////////////////////////////////////////////////////////////
